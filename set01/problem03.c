5851b4370e965af000edac6f2c84fd0eb4b97dff
prabhasbhat <114654634+prabhasbhat@users.noreply.github.com>
2022-11-30 10:29:22 +0530

    used input() function

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 int main(void)
 {
-  int a,b,sum;
-  printf("Enter the first number\n");
-  scanf("%d",&a);
-  printf("Enter the second number\n");
-  scanf("%d",&b);
+  int sum,a,b;
+  a=input();
+  b=input();
   sum=sum_func(a,b);
   void output(int a, int b, int sum);
   printf("The sum of %d and %d is %d",a,b,sum);
   return(0);
+}
+int input()
+{
+  int a;
+  printf("Enter the number\n");
+  scanf("%d",&a); 
+  return a;
 }
 sum_func(int a,int b)
 {
